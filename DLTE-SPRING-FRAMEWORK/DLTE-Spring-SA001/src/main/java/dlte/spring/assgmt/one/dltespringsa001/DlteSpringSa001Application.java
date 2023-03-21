0d2faa523ea439594fc63c6be9d26c3519a33796
Shrinidhi Shervegar <shrinidhirao17@gmail.com>
2023-03-21 15:51:31 +0530

    Add files via upload

---
+++ DLTE-SPRING-FRAMEWORK/DLTE-Spring-SA001/src/main/java/dlte/spring/assgmt/one/dltespringsa001/DlteSpringSa001Application.java
@@ -0,0 +1,13 @@
+package dlte.spring.assgmt.one.dltespringsa001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlteSpringSa001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DlteSpringSa001Application.class, args);
+    }
+
+}
---
+++ DLTE-SPRING-FRAMEWORK/DLTE-Spring-SA001/src/test/java/dlte/spring/assgmt/one/dltespringsa001/DlteSpringSa001ApplicationTests.java
@@ -0,0 +1,13 @@
+package dlte.spring.assgmt.one.dltespringsa001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlteSpringSa001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
